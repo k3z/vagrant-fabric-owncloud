7b20ecde8d6b20945de22fe20610dde958dbcf51
Stéphane Klein <sklein@bearstech.com>
2013-01-24 10:09:32 +0100

    Fix small bug if /root/.ssh/ already exists

--- append_authorized_keys_to_root.sh
+++ append_authorized_keys_to_root.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo mkdir /root/.ssh
-sudo cp .ssh/authorized_keys /root/.ssh/
-sudo chown root:root /root/.ssh/authorized_keys
+if [ ! -d /root/.ssh ]
+then
+    sudo mkdir /root/.ssh
+    sudo cp .ssh/authorized_keys /root/.ssh/
+    sudo chown root:root /root/.ssh/authorized_keys
+fi
